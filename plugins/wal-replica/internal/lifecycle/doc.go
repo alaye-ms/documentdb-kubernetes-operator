12b06cfbed14b42060125d1b8754a78399961c8d
Alexander Laye <alaye@microsoft.com>
2025-09-05 16:37:43 -0400

    remove lifecycle

--- plugins/wal-replica/internal/lifecycle/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-// Package lifecycle implements the lifecycle hooks
-package lifecycle
--- plugins/wal-replica/internal/reconciler/replica.go
+++ plugins/wal-replica/internal/reconciler/replica.go
@@ -3,13 +3,18 @@ package reconciler
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	apiv1 "github.com/cloudnative-pg/api/pkg/api/v1"
-	"github.com/cloudnative-pg/cnpg-i/pkg/lifecycle"
+	"github.com/cloudnative-pg/cnpg-i-machinery/pkg/pluginhelper/common"
 	"github.com/cloudnative-pg/machinery/pkg/log"
 	"github.com/documentdb/cnpg-i-wal-replica/internal/config"
 	"github.com/documentdb/cnpg-i-wal-replica/internal/k8sclient"
+	"github.com/documentdb/cnpg-i-wal-replica/pkg/metadata"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
@@ -20,83 +25,141 @@ func CreateWalReplica(
 ) error {
 	logger := log.FromContext(ctx).WithName("CreateWalReplica")
 
-	// Build Deployment name unique per cluster
-	depName := fmt.Sprintf("%s-wal-receiver", cluster.Name)
-	namespace := cluster.Namespace
-	cl := k8sclient.MustGet()
-
-	// derive primary host if not provided: <cluster>-rw (CNPG service naming) or cluster.Status.CurrentPrimary
-	replHost := configuration.ReplicationHost
-	if replHost == "" {
-		if cluster.Status.CurrentPrimary != "" {
-			// cluster.Status.CurrentPrimary is in the form <cluster>-<podIdx>
-			// Postgres primary service in CNPG is <cluster>-rw
-			replHost = fmt.Sprintf("%s-rw", cluster.Name)
-		} else {
-			replHost = fmt.Sprintf("%s-rw", cluster.Name)
-		}
+	if !IsPrimaryCluster(cluster) {
+		logger.Info("Cluster is not a primary, skipping wal replica creation", "cluster", cluster.Name)
+		return nil
 	}
 
+	// Build Deployment name unique per cluster
+	deploymentName := fmt.Sprintf("%s-wal-receiver", cluster.Name)
+	namespace := cluster.Namespace
+	client := k8sclient.MustGet()
+
+	helper := common.NewPlugin(
+		*cluster,
+		metadata.PluginName,
+	)
+
+	configuration := config.FromParameters(helper)
+
 	walDir := configuration.WalDirectory
-	cmd := []string{"/usr/bin/pg_receivewal", "--no-loop", "--directory", walDir, "--host", replHost, "--user", "streaming_replica"}
+	cmd := []string{
+		"/usr/bin/postgresql/16/bin/pg_receivewal", // TODO find the actual path
+		"--slot", "wal_replica",
+		"--compress", "0",
+		"--directory", walDir,
+		"--host", configuration.ReplicationHost,
+		"--port", "5432",
+		"--username", "postgres",
+		"--no-password",
+		"--verbose",
+	}
 
 	// Add synchronous flag if requested
 	if configuration.Synchronous == config.SynchronousActive {
 		cmd = append(cmd, "--synchronous")
 	}
 
+	// Create a pVC
+	// Needs a PVC to store the wal data
+	existingPVC := &corev1.PersistentVolumeClaim{}
+	err := client.Get(ctx, types.NamespacedName{Name: deploymentName, Namespace: namespace}, existingPVC)
+	if err != nil && errors.IsNotFound(err) {
+		log.Info("WAL replica PVC not found. Creating a new WAL replica PVC")
+
+		walReplicaPVC := &corev1.PersistentVolumeClaim{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      deploymentName,
+				Namespace: namespace,
+			},
+			Spec: corev1.PersistentVolumeClaimSpec{
+				AccessModes: []corev1.PersistentVolumeAccessMode{
+					corev1.ReadWriteOnce,
+				},
+				Resources: corev1.VolumeResourceRequirements{
+					Requests: corev1.ResourceList{
+						corev1.ResourceStorage: resource.MustParse("10Gi"),
+					},
+				},
+			},
+		}
+
+		err = client.Create(ctx, walReplicaPVC)
+		if err != nil {
+			return err
+		}
+	} else if err != nil {
+		return err
+	}
+
 	// Create or patch Deployment
 	existing := &appsv1.Deployment{}
-	err = cl.Get(ctx, types.NamespacedName{Name: depName, Namespace: namespace}, existing)
+	err = client.Get(ctx, types.NamespacedName{Name: deploymentName, Namespace: namespace}, existing)
 	if err != nil {
 		dep := &appsv1.Deployment{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      depName,
+				Name:      deploymentName,
 				Namespace: namespace,
 				Labels: map[string]string{
-					"app":             depName,
+					"app":             deploymentName,
 					"cnpg.io/cluster": cluster.Name,
 				},
 			},
 			Spec: appsv1.DeploymentSpec{
-				Selector: &metav1.LabelSelector{MatchLabels: map[string]string{"app": depName}},
+				Selector: &metav1.LabelSelector{MatchLabels: map[string]string{"app": deploymentName}},
 				Template: corev1.PodTemplateSpec{
-					ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"app": depName}},
+					ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"app": deploymentName}},
 					Spec: corev1.PodSpec{
 						Containers: []corev1.Container{{
-							Name:           "wal-receiver",
-							Image:          configuration.Image,
-							Args:           cmd,
-							Ports:          []corev1.ContainerPort{{Name: "metrics", ContainerPort: 9187}},
-							ReadinessProbe: &corev1.Probe{ProbeHandler: corev1.ProbeHandler{Exec: &corev1.ExecAction{Command: []string{"/bin/sh", "-c", "test -d " + walDir}}}, PeriodSeconds: 30},
+							Name:  "wal-receiver",
+							Image: configuration.Image,
+							Args:  cmd,
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      deploymentName,
+									MountPath: walDir,
+								},
+							},
 						}},
+						Volumes: []corev1.Volume{
+							{
+								Name: deploymentName,
+								VolumeSource: corev1.VolumeSource{
+									PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+										ClaimName: deploymentName,
+									},
+								},
+							},
+						},
+						SecurityContext: &corev1.PodSecurityContext{
+							RunAsUser:  int64Ptr(105),
+							RunAsGroup: int64Ptr(103),
+							FSGroup:    int64Ptr(103),
+						},
 						RestartPolicy: corev1.RestartPolicyAlways,
 					},
 				},
 			},
 		}
 		// optional service for metrics
-		svc := &corev1.Service{ObjectMeta: metav1.ObjectMeta{Name: depName, Namespace: namespace, Labels: map[string]string{"app": depName}}, Spec: corev1.ServiceSpec{Selector: map[string]string{"app": depName}, Ports: []corev1.ServicePort{{Name: "metrics", Port: 9187, TargetPort: intstr.FromInt(9187)}}}}
-		if createErr := cl.Create(ctx, dep); createErr != nil {
+		svc := &corev1.Service{ObjectMeta: metav1.ObjectMeta{Name: deploymentName, Namespace: namespace, Labels: map[string]string{"app": deploymentName}}, Spec: corev1.ServiceSpec{Selector: map[string]string{"app": deploymentName}, Ports: []corev1.ServicePort{{Name: "metrics", Port: 9187, TargetPort: intstr.FromInt(9187)}}}}
+		if createErr := client.Create(ctx, dep); createErr != nil {
 			logger.Error(createErr, "creating wal receiver deployment")
-			return nil, createErr
+			return createErr
 		}
-		_ = cl.Create(ctx, svc) // ignore error if exists
-		logger.Info("created wal receiver deployment", "name", depName)
+		_ = client.Create(ctx, svc) // ignore error if exists
+		logger.Info("created wal receiver deployment", "name", deploymentName)
 	} else {
-		// patch spec if needed
-		updated := existing.DeepCopy()
-		if len(updated.Spec.Template.Spec.Containers) == 0 || !strings.EqualFold(updated.Spec.Template.Spec.Containers[0].Image, configuration.Image) || !equalStringSlices(updated.Spec.Template.Spec.Containers[0].Args, cmd) {
-			updated.Spec.Template.Spec.Containers = []corev1.Container{existing.Spec.Template.Spec.Containers[0]}
-			updated.Spec.Template.Spec.Containers[0].Image = configuration.Image
-			updated.Spec.Template.Spec.Containers[0].Args = cmd
-			if patchErr := cl.Update(ctx, updated); patchErr != nil {
-				logger.Error(patchErr, "updating wal receiver deployment")
-				return nil, patchErr
-			}
-			logger.Info("updated wal receiver deployment", "name", depName)
-		}
+		// TODO handle patch
 	}
 
-	return &lifecycle.OperatorLifecycleResponse{}, nil
+	return nil
+}
+func int64Ptr(i int64) *int64 {
+	return &i
+}
+
+func IsPrimaryCluster(cluster *apiv1.Cluster) bool {
+	// TODO implement
+	return true
 }
